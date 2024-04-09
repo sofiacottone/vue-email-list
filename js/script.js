578b39bc045ec79e5a2f3a2d1a628b70e2dbbfca
sofiacottone <cottonesofia@outlook.it>
2024-04-09 15:00:52 +0200

    init project

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// ISTRUZIONI
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+        }
+    },
+    methods: {
+    },
+    mounted() {
+    }
+}).mount('#app');
